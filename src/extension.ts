1859d6b8afd9b32d53467d0690a3578daaf00cf9
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-04-12 21:57:34 -0500

    feat: population of test nodes with their test cases.

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 import *  as path from 'path';
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
 
     const testCtrl = vscode.tests.createTestController('busted-tests', 'Busted Tests');
     context.subscriptions.push(testCtrl);
 
-    const getTestNode = (uri: vscode.Uri) => {
+    const getTestNode = (uri: vscode.Uri, create: boolean = true) => {
 
         const workspace = vscode.workspace.getWorkspaceFolder(uri);
         if (!workspace) { return; }
@@ -17,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
         if (relativePath.includes("/.")) { return; }
 
         // Create a branch of testItems if necessary.
-        let testNode;
+        let testNode: vscode.TestItem | undefined;
         let nodeGroup = testCtrl.items;
         let nodeId = path.dirname(workspace.uri.path);
         const tokens = relativePath.split("/");
@@ -25,6 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
             nodeId = path.join(nodeId, token);
             testNode = nodeGroup.get(nodeId);
             if (!testNode) {
+                if (!create) { return; }
                 const nodeUri = vscode.Uri.file(nodeId);
                 testNode = testCtrl.createTestItem(nodeId, token, nodeUri);
                 nodeGroup.add(testNode);
@@ -34,24 +36,29 @@ export function activate(context: vscode.ExtensionContext) {
         return testNode;
     };
 
+    const dropTestNode = (uri: vscode.Uri) => {
+        const testNode = getTestNode(uri, false);
+        if (!testNode) { return; }
+        if (!testNode.parent) {
+            testCtrl.items.delete(uri.path);
+            return;
+        }
+        testNode.parent.children.delete(uri.path);
+    };
+
     const watchWorkspace = async (workspace: vscode.WorkspaceFolder) => {
 
         const pattern = new vscode.RelativePattern(workspace, '**/*_spec.lua');
         const watcher = vscode.workspace.createFileSystemWatcher(pattern);
 
-        watcher.onDidCreate(uri => getTestNode(uri));
+        watcher.onDidCreate(uri => parseTestNode(getTestNode(uri)));
 
-        // TODO: Delete node when its file is deleted.
-        // watcher.onDidDelete(uri => getTestNode(uri));
+        watcher.onDidDelete(uri => dropTestNode(uri));
 
-        // TODO: Parse tests inside the node file when it is changed.
-        // watcher.onDidChange(uri => toDoFunction ( getTestNode(uri) ));
+        watcher.onDidChange(uri => parseTestNode(getTestNode(uri)));
 
         for (const file of await vscode.workspace.findFiles(pattern)) {
-            const testNode = getTestNode(file);
-            if (testNode) {
-                // TODO: Parse tests inside the node file.
-            }
+            parseTestNode(getTestNode(file));
         }
 
         context.subscriptions.push(watcher);
@@ -68,13 +75,83 @@ export function activate(context: vscode.ExtensionContext) {
     };
 
 
+    // Whether to resolve all workspaces, or just a particular node.
     testCtrl.resolveHandler = async (test) => {
         if (!test) {
             await watchAllWorkspaces();
         } else {
-            // TODO: Parse tests inside the node file.
+            await parseTestNode(test);
         }
     };
+
+    // Parses an output line to retrive their aproximate row and label.
+    const parseMatch = (text: string): [number, string] => {
+
+        const pattern = /([^\/]+(\/[^\/:]+)+_spec.lua):(\d+): (.+)/;
+        const match = pattern.exec(text);
+
+        if (!match) { return [1, text]; }
+
+        const row = parseInt(match[3]);
+        const name = match[4].trim();
+
+        return [row, name];
+    };
+
+    // Generate test cases given an output line from the list command.
+    const getTestCase = (text: string, test: vscode.TestItem) => {
+
+        const [row, label] = parseMatch(text);
+        const testId = test.uri!.path + ":" + label;
+
+        const existing = test.children.get(testId);
+        if (existing) { return; }
+
+        const testCase = testCtrl.createTestItem(testId, label, test.uri);
+        test.children.add(testCase);
+
+        const position = new vscode.Position(row - 1, 0);
+        testCase.range = new vscode.Range(position, position);
+
+        return testCase;
+    };
+
+    // Executes 'busted --list <spec file>' to discover test cases.
+    const parseTestNode = async (test: vscode.TestItem | undefined) => {
+
+        if (!test?.uri) { return; }
+
+        const workspace = vscode.workspace.getWorkspaceFolder(test.uri);
+        if (!workspace) { return; }
+
+        const options = { 'cwd': workspace.uri.path };
+        const process = spawn('busted', ['--list', test.uri.path], options);
+
+        process.stdout.on('data', data => {
+            const text = String.fromCharCode(...data).trim();
+            text.split('\n').forEach(line => getTestCase(line, test));
+        });
+    };
+
+    const parseTestsInDocument = (doc: vscode.TextDocument) => {
+        if (doc.uri.scheme !== 'file') { return; }
+        if (!doc.uri.path.endsWith('_spec.lua')) { return; }
+        parseTestNode(getTestNode(doc.uri));
+    };
+
+    context.subscriptions.push(
+        vscode.workspace.onDidOpenTextDocument(parseTestsInDocument),
+        vscode.workspace.onDidChangeTextDocument(
+            event => parseTestsInDocument(event.document)
+        )
+    );
+
+    // Covers the case of test modules deletion from within VSCode.
+    context.subscriptions.push(
+        vscode.workspace.onDidDeleteFiles(
+            event => event.files.forEach(dropTestNode)
+        )
+    );
 }
 
 export function deactivate() { }
