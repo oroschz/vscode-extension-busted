bdf852638e83af7ab186b9d7756ef21c32e24835
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-04-13 15:21:15 -0500

    feat: run tests from the test explorer.

--- src/extension.ts
+++ src/extension.ts
@@ -156,6 +156,141 @@ export function activate(context: vscode.ExtensionContext) {
             event => event.files.forEach(dropTestNode)
         )
     );
+
+    // Updates the tests run results based on busted json response.
+    const updateRunResults = (results: any, run: vscode.TestRun, group: vscode.TestItem) => {
+
+        const { successes, failures, pendings, errors } = results;
+
+        const states = [successes, failures, pendings, errors];
+
+        for (const state of states) {
+            for (const result of state) {
+
+                const caseId = getTestCaseId(group.uri!, result.name);
+                const testCase = group.children.get(caseId);
+
+                if (!testCase) { continue; }
+
+                const duration = 1000 * result.element.duration;
+                const message = new vscode.TestMessage(result.trace.traceback);
+                switch (state) {
+                    case successes:
+                        run.passed(testCase, duration);
+                        break;
+                    case failures:
+                        run.failed(testCase, message, duration);
+                        break;
+                    case pendings:
+                        run.skipped(testCase);
+                        break;
+                    case errors:
+                        run.errored(testCase, message, duration);
+                        break;
+                }
+            }
+        }
+
+        const duration = 1000 * results.duration;
+        if (failures.length > 0) {
+            const message = new vscode.TestMessage("");
+            run.failed(group, message, duration);
+        } else if (errors.length > 0) {
+            const message = new vscode.TestMessage("");
+            run.errored(group, message, duration);
+        } else if (pendings.length > 0) {
+            run.skipped(group);
+        } else {
+            run.passed(group, duration);
+        }
+    };
+
+    const runHandler = async (request: vscode.TestRunRequest, token: vscode.CancellationToken) => {
+
+        const run = testCtrl.createTestRun(request);
+        const _queue: vscode.TestItem[] = [];
+
+        if (request.include) {
+            request.include.forEach((test) => _queue.push(test));
+        } else {
+            testCtrl.items.forEach((test) => _queue.push(test));
+        }
+
+        // TODO: Implement a better alternative to this algorithm.
+        const queue = [];
+        for (const item of _queue) {
+            run.enqueued(item);
+            if (item.uri?.path.includes('.lua')) {
+                queue.push(item);
+                item.children.forEach(child => run.enqueued(child));
+                continue;
+            }
+            item.children.forEach(child => _queue.push(child));
+        }
+
+        const runTestFile = async (test: vscode.TestItem) => {
+
+            // TODO: Review the need for these check guards.
+            if (request.exclude?.includes(test) || token.isCancellationRequested) {
+                return;
+            }
+
+            if (!test.uri) {
+                console.log("Uri not found for test");
+                return;
+            }
+
+            // TODO: This promise could be handled better.
+            return new Promise<void>((resolve, reject) => {
+
+                const cwd = vscode.workspace.getWorkspaceFolder(test.uri!);
+                const options = { cwd: cwd!.uri.path };
+                const child = spawn('busted', ['-o', 'json', test.uri!.path], options);
+
+                token.onCancellationRequested(child.kill);
+
+                // TODO: There may probably be other alternatives to buffering stdout.
+                let output = "";
+                child.stdout.on('data', data => { output += data; });
+
+                child.stderr.on('data', data => { console.error(data); });
+
+                child.on('error', error => {
+                    vscode.window.showErrorMessage(error.message);
+                });
+
+                child.on('close', code => {
+                    if (child.killed) {
+                        return;
+                    }
+                    try {
+                        const results = JSON.parse(output);
+                        updateRunResults(results, run, test);
+                    } catch {
+                        const message = new vscode.TestMessage("Error parsing json input.");
+                        run.errored(test, message);
+                        vscode.window.showErrorMessage(`${test.id}: ${message.message}`);
+                    }
+                    resolve();
+                });
+
+                run.started(test);
+            });
+        };
+
+        // Run tests files sequencially
+        // for (const test of queue) { await runTestFile(test); }
+
+        // Run tests files concurrently
+        await Promise.allSettled(queue.map(runTestFile));
+
+        run.end();
+    };
+
+    context.subscriptions.push(
+        testCtrl.createRunProfile('Run', vscode.TestRunProfileKind.Run, runHandler),
+        testCtrl.createRunProfile('Debug', vscode.TestRunProfileKind.Debug, runHandler)
+    );
 }
 
 export function deactivate() { }
